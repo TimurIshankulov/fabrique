8dbb259167dffb3d188419f6dff0959f3fdaa3e2
Timur Ishankulov <tim290592@gmail.com>
2022-08-09 16:31:26 +0300

    Update validators

--- api/validators.py
+++ api/validators.py
@@ -1,5 +1,6 @@
 from django.core.validators import RegexValidator
 
-phone_number_regex = RegexValidator(regex=r'7\d{10}',
+# Validator for phone number
+phone_number_regex = RegexValidator(regex=r'^7\d{10}$',
                                     message='Incorrect phone number',
                                     code='incorrect_phone_number')
