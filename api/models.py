61cb4a4d773945ce36052f55e33bcbf8f6900b87
Timur Ishankulov <tim290592@gmail.com>
2022-08-09 16:30:35 +0300

    Update models

--- api/models.py
+++ api/models.py
@@ -4,14 +4,17 @@ from .validators import phone_number_regex
 
 
 class Mailing(models.Model):
+    """Class describes mailing model"""
     unique_id = models.IntegerField(null=False, unique=True)
     datetime_start = models.DateTimeField()
     message_text = models.TextField()
     filter_property = models.CharField(max_length=30)
     datetime_end = models.DateTimeField()
+    status = models.CharField(max_length=30, default='new')
 
 
 class Client(models.Model):
+    """Class describes client model"""
     unique_id = models.IntegerField(null=False, unique=True)
     phone_number = models.CharField(max_length=20, validators=[phone_number_regex])
     provider_code = models.CharField(max_length=10)
@@ -20,8 +23,9 @@ class Client(models.Model):
 
 
 class Message(models.Model):
-    unique_id = models.IntegerField(null=False, unique=True)
-    datetime_sent = models.DateTimeField()
+    """Class describes message model"""
+    unique_id = models.IntegerField(null=True, unique=True)
+    datetime_sent = models.DateTimeField(null=True)
     status = models.CharField(max_length=30)
-    mailing_id = models.ForeignKey(Mailing, on_delete=models.CASCADE, to_field='unique_id')
-    client_id = models.ForeignKey(Client, on_delete=models.CASCADE, to_field='unique_id')
+    mailing = models.ForeignKey(Mailing, on_delete=models.CASCADE, to_field='unique_id')
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, to_field='unique_id')
